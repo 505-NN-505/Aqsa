15bcbb919ae02a488e8b2037ff699ca4a590fe5e
jubayer <jubayerislam@iut-dhaka.edu>
2023-06-07 12:42:38 +0600

    Revert "added files from my own project directories"
    This reverts commit d0c4484dbeea726f3544479ec48c87df3dc5a213.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,26 @@
-import 'package:aqsa_muslim_prayer_assistant/main_page.dart';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
+import 'model/api_testing.dart';
+
+
 void main() {
-  runApp(const MainPage());
+  runApp(const HomePage());
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ApiTesting(),
+    );
+  }
 }
